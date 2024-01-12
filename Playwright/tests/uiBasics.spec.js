a8ce68a2ba88349aee40a999b93cc8b7dcbfffd8
Dipanshu <>
2024-01-12 19:03:02 +0530

    added assertion

--- Playwright/tests/uiBasics.spec.js
+++ Playwright/tests/uiBasics.spec.js
@@ -1,12 +1,17 @@
-const { test } = require("@playwright/test");
+const { test,expect } = require("@playwright/test");
 
 test("Browser contxt test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto("https://www.google.com");
+  console.log(await page.title());
+  await expect(page).toHaveTitle('Google');
+
 });
 
 //if we don't have any specific context defined then we can use page fixture directly
 test("page contxt test", async ({ page }) => {
   await page.goto("https://www.google.com");
+  console.log(await page.title());
+  await expect(page).toHaveTitle('Google');
 });
